4528524257c215cd292d9d4d735f30cd6f9c0e05
Janita <janita_irfan7@yahoo.com>
2018-08-02 16:30:25 +0100

    Add files via upload

---
+++ HajjEventBus/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajjEventBus
+{
+    public class Class1
+    {
+    }
+}
---
+++ HajjEventBus/IEventBusSubscriptionsManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajjEventBus
+{
+    public interface IEventBusSubscriptionsManager
+    {
+        bool IsEmpty { get; }
+        event EventHandler<string> OnEventRemoved;      
+    }
+}
